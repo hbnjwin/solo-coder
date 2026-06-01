72ea54a981f72b31e609fdf33bc54961adb43d92
hbnjwin <33817103+hbnjwin@users.noreply.github.com>
2026-06-01 17:52:42 +0800

    init: hcmsp-stats-report base project

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+﻿<template>
+  <el-container>
+    <el-header>
+      <h1>hcmsp-stats-report</h1>
+    </el-header>
+    <el-main>
+      <router-view />
+    </el-main>
+  </el-container>
+</template>
+
+<script setup>
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif;
+}
+</style>
